6a9fd8a71bca715c72b64230ef4cd5b35729ad1b
Kevin Bai <lkevin957@gmail.com>
2019-03-14 10:36:09 -0700

    Add babel to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,16 @@ module.exports = {
                     'style-loader',
                     'css-loader'
                 ]
+            },
+            {
+                test: [/\.jsx?$/], // Specifies file types to transpile
+                exclude: /(node_modules)/, // Leaves dependencies alone
+                loader: 'babel-loader', // Sets Babel as the transpiler
+                query: {
+                    presets: ['@babel/env', '@babel/react'] // Tells Babel what syntaxes to translate
+                }
             }
         ]
-    }
+    },
+    devtool: 'source-map'
 };
