f90845c5f65515f7824c4cb1aa1d5cb06ffc47e2
Kevin Bai <lkevin957@gmail.com>
2019-03-15 13:32:24 -0700

    Add layout file to set how data should arranged to be passed into model

---
+++ src/data/Layout.js
@@ -0,0 +1,27 @@
+    let salaryIncome = 75000;
+    let investmentReturnIncome = 5000;
+
+    let incomeSavings = 12000;
+    let incomeInvestments = 4000;
+    let incomeExpenses = 36000;
+    let incomeTaxes = 28000;
+
+    let savingsSaved = 12000;
+    let investmentsSaved = 4000;
+    let expensesSpent = 36000;
+    let taxesSpent = 28000;
+
+    const object = objectMaker(
+        salaryIncome,
+        investmentReturnIncome,
+
+        incomeSavings,
+        incomeInvestments,
+        incomeExpenses,
+        incomeTaxes,
+
+        savingsSaved,
+        investmentsSaved,
+        expensesSpent,
+        taxesSpent
+    );
