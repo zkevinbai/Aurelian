1e8bbce51a8d8989438219fec005acbad1de1b37
Kevin Bai <lkevin957@gmail.com>
2019-03-15 11:51:18 -0700

    Update index to have fields based on generator requirements

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import objectMaker from './objectGenerator';
 import urlMaker from './urlGenerator';
 
 document.addEventListener("DOMContentLoaded", ()=>{
+    let salaryIncome = 0
+    let investmentReturnIncome = 0
+    let incomeExpenses = 0
+    let incomeTaxes = 0
+    let incomeSavings = 0
+    let incomeInvestments = 0
+    let expensesSpent = 0
+    let taxesSpent = 0
+    let savingsSaved = 0
+    let investmentsSaved = 0
+
     const object = objectMaker();
     const url = urlMaker(object);
     visualization(url)
