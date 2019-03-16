a70c2913d3f9f0097882f982573f661eaa5d2fb8
Kevin Bai <lkevin957@gmail.com>
2019-03-15 20:36:48 -0700

    Reorganize backup files

---
+++ src/backup/data/oldDataGenerator.js
@@ -0,0 +1,57 @@
+// export const defaultData = () => {
+//     return [
+//         75000,
+//         5000,
+//         12000,
+//         4000,
+//         36000,
+//         28000,
+//         12000,
+//         4000,
+//         36000,
+//         28000,
+//     ]
+// }
+
+// let salaryIncome = 75000;
+// let investmentReturnIncome = 5000;
+
+// let incomeSavings = 12000;
+// let incomeInvestments = 4000;
+// let incomeExpenses = 36000;
+// let incomeTaxes = 28000;
+
+// let savingsSaved = 12000;
+// let investmentsSaved = 4000;
+// let expensesSpent = 36000;
+// let taxesSpent = 28000;
+
+// export const dataMaker = (
+//     salaryIncome,
+//     investmentReturnIncome,
+
+//     incomeSavings,
+//     incomeInvestments,
+//     incomeExpenses,
+//     incomeTaxes,
+
+//     savingsSaved,
+//     investmentsSaved,
+//     expensesSpent,
+//     taxesSpent
+// ) => {
+//     return [
+//     salaryIncome,
+//     investmentReturnIncome,
+
+//     incomeSavings,
+//     incomeInvestments,
+//     incomeExpenses,
+//     incomeTaxes,
+
+//     savingsSaved,
+//     investmentsSaved,
+//     expensesSpent,
+//     taxesSpent
+//     ]
+// }
